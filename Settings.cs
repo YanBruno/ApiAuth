068dc150b6010c2bc8b6803ea4ed53c5723fb995
yan.santos <yan.santos@midway.com.br>
2021-10-20 23:30:28 -0300

    cometario no Settings

--- Settings.cs
+++ Settings.cs
@@ -1,5 +1,6 @@
 namespace ApiAuth
 {
+    //Este arquivo faria parte do .gitignore caso não fosse um projeto de estudo
     public static class Settings{
         public static string SecretTokenKey = "banana8432537c78b34ec0933b98c183fdee2e";
     }
